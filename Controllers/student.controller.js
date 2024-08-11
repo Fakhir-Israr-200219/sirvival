c926d5d85e225910e73099814a0b124efeb4eac1
Fakhir-Israr-200219 <hmfakhirisrar@gmail.com>
2024-08-10 18:44:50 -0700

    crud compleate

--- Controllers/student.controller.js
+++ Controllers/student.controller.js
@@ -1,32 +1,55 @@
 const expressAsyncHandler = require("express-async-handler");
 const studentModel = require("../Models/student.model");
-
-const AddStudent = expressAsyncHandler(async (req,res)=>{
-    const {name , email , phone } = req.body;
-    if(!name && !email && !phone){
+const { ObjectId } = require("mongodb");
+const AddStudent = expressAsyncHandler(async (req, res) => {
+    const { name, email, phone } = req.body;
+    if (!name && !email && !phone) {
         res.status(404);
         throw new Error("name email phone is mandotory");
     }
     const student = await studentModel.create(req.body);
-    res.status(200).json({student});
+    res.status(200).json({ student });
 
 })
 
-const getAllStudent = expressAsyncHandler(async(req,res)=>{
+const getAllStudent = expressAsyncHandler(async (req, res) => {
     const allStudent = await studentModel.find();
-    res.status(200).json({allStudent});
+    res.status(200).json({ allStudent });
 })
 
 
-const getStudentById = expressAsyncHandler(async(req,res)=>{
+const getStudentById = expressAsyncHandler(async (req, res) => {
     const student = await studentModel.findById(req.params.id)
-    if(!student){
+    if (!student) {
         res.status(404); // Set the response status to 404 (Not Found)
         throw new Error("student not found");
     }
-    res.status(200).json({student})
+    res.status(200).json({ student })
 })
 
+
+const updatdStudent = expressAsyncHandler(async (req, res) => {
+    const student = await studentModel.findById(req.params.id)
+    if (!student) {
+        res.status(404);
+        throw new Error();
+    }
+    const updatedStudent = await studentModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json({ updatedStudent })
+})
+
+const deleteStudent = expressAsyncHandler(async (req, res) => {
+    const student = await studentModel.findById(req.params.id)
+    if (!student) {
+        res.status(404); // Set the response status to 404 (Not Found)
+        throw new Error("student not found");
+    }
+    const data = await studentModel.deleteOne(new ObjectId(req.params.id));
+    res.status(200).json(data);
+
+})
+
+
 module.exports = {
-    AddStudent,getAllStudent,getStudentById
+    AddStudent, getAllStudent, getStudentById, updatdStudent, deleteStudent
 }
--- Routes/student.route.js
+++ Routes/student.route.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const {getAllStudent,getStudentById,AddStudent} = require("../Controllers/student.controller");
+const {getAllStudent,getStudentById,AddStudent,updatdStudent,deleteStudent} = require("../Controllers/student.controller");
 const router = express.Router();
 
 router.get("/",getAllStudent);
 router.get("/:id",getStudentById);
 router.post("/",AddStudent);
+router.delete("/:id",deleteStudent);
+router.put("/:id",updatdStudent);
 
 module.exports = router
