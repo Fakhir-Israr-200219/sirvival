4cfa6beef12559bbe8e7525b7a41020979cace08
Fakhir-Israr-200219 <hmfakhirisrar@gmail.com>
2024-08-15 00:12:56 -0700

    Compleate

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,27 +1,28 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 const User = require("../Models/userModel")
+const jwt = require("jsonwebtoken")
 
 const register = asyncHandler(async (req, res) => {
     const { userName, email, password } = req.body;
-    if(!userName && !email && !password){
+    if (!userName && !email && !password) {
         res.status(404);
         throw new Error("all feailds are mandatory")
     }
-    const avalibleUser = await User.findOne({userName});
-    if(avalibleUser){
+    const avalibleUser = await User.findOne({ userName });
+    if (avalibleUser) {
         res.status(400);
         throw new Error("User name already taken");
     }
-    const hashPassword = await bcrypt.hash(password,10);
+    const hashPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
         userName,
         email,
-        password :hashPassword,
+        password: hashPassword,
     });
-    if(user){
-        res.status(201).json({_id : user.id ,email :user.email})
-    }else{
+    if (user) {
+        res.status(201).json({ _id: user.id, email: user.email })
+    } else {
         res.status(400)
         throw new Error("User data is not valid")
     }
@@ -29,14 +30,27 @@ const register = asyncHandler(async (req, res) => {
 
 
 const login = asyncHandler(async (req, res) => {
-    const {userName , password } = req.body;
-    if(!userName && !password){
+    const { userName, password } = req.body;
+    if (!userName && !password) {
         res.status(400);
         throw new Error("user name aand password is required")
     }
-    const user = await User.findOne({userName});
-    if(user && bcrypt.compare(password,user.password)){
-        // const acessToken = 
+    const user = await User.findOne({ userName });
+    if (user && bcrypt.compare(password, user.password)) {
+        const acessToken = jwt.sign({
+            user: {
+                userName: user.userName,
+                email: user.email,
+                password: user.password,
+            },
+        },
+            process.env.VALIDATE_TOKEN_SECREAT,
+            { expiresIn: "15m" }
+        );
+        res.status.json({ acessToken })
+    } else {
+        res.status(401);
+        throw new Error("email or password is not valid");
     }
 
 })
@@ -44,7 +58,7 @@ const login = asyncHandler(async (req, res) => {
 
 
 const currentUser = asyncHandler(async (req, res) => {
-    res.status(200).json("currentUser hit the route successflly")
+    res.status(200).json(req.user);
 })
 
 module.exports = {
